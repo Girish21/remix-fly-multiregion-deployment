3bb2411ad824cc0f94c5efd2109b707b3cb863b9
Girish21 <girishkumarv21@gmail.com>
2022-02-10 01:02:23 +0530

    chore: add global types

--- app/types.ts
+++ app/types.ts
@@ -3,4 +3,25 @@ type GitHubFile = {
   path: string;
 };
 
-export type { GitHubFile };
+type MdxPage = {
+  code: string;
+  slug: string;
+  frontmatter: {
+    published?: boolean;
+    title?: string;
+    description?: string;
+    meta?: Record<string, string | string[]>;
+    date?: string;
+  };
+};
+
+type MdxComponent = {
+  frontmatter: Record<string, unknown>;
+  slug: string;
+  title: string;
+  code: string;
+  timestamp: Date;
+  updatedAt: Date;
+};
+
+export type { GitHubFile, MdxPage, MdxComponent };

eae532d971bed484ab16faa14ba20ad9bd45652c
Girish21 <girishkumarv21@gmail.com>
2022-02-10 23:46:52 +0530

    chore: fix impossible state and add docs

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -113,24 +113,59 @@ export async function getMdxListItems({
   return getContentList();
 }
 
+/**
+ * function which returns the compiled MDX page
+ * and the meta info for a given slug and the
+ * content directory.
+ *
+ * If the page is not pre compiled or requires update,
+ * the page is fetched, compiled and returned
+ *
+ * The refreshed page is not persisted in DB
+ * because of a corner case. Let's say you deploy
+ * the app to a new Fly region which has a fresh
+ * data store. If the user vists the page directly via
+ * a link, since we don't have that page in our cache,
+ * we go and download the page, compile MDX and now
+ * if we try to persist it in our DB, we will end up
+ * in a situation where we might not be able to fetch the
+ * rest of the pages when we vist the index route.
+ *
+ * Because we don't store the total count of pages,
+ * there is no way to know if we have downloaded and
+ * cached all the pages.
+ *
+ * You may as why not have a count. Yeah we can, but hey
+ * this is a simple starter to get started
+ * and feel free to rewrite this block as per you requirement :).
+ */
 export async function getMdxPage({
   slug,
   contentDirectory,
 }: {
   slug: string;
   contentDirectory: string;
-}) {
+}): ReturnType<typeof getContent> {
   const data = await getContent(slug);
 
   if (data && !data.requiresUpdate) {
     return data;
   }
 
-  if (!data || data.requiresUpdate) {
-    const pages = await downloadMdx([{ slug }], contentDirectory);
-    const compiledPages = await compileMdxPages(pages);
-    await upsertContent(compiledPages, contentDirectory);
+  const pages = await downloadMdx([{ slug }], contentDirectory);
+  const [compiledPage] = await compileMdxPages(pages);
 
-    return getContent(slug);
+  if (!compiledPage) {
+    throw new Error(`Page ${slug} could not be compiled`);
   }
+
+  return {
+    code: compiledPage.code,
+    contentDirectory,
+    frontmatter: compiledPage.frontmatter,
+    requiresUpdate: false,
+    slug,
+    timestamp: new Date(compiledPage.frontmatter.date ?? ""),
+    title: compiledPage.frontmatter.title ?? "",
+  };
 }

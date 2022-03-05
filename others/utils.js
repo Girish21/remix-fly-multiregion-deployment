14065639510d0be9fe79a8e801b4c1cfa9ec3dd5
Girish21 <girishkumarv21@gmail.com>
2022-03-05 16:16:39 +0530

    chore: handle request errors

--- others/utils.js
+++ others/utils.js
@@ -2,21 +2,28 @@ const execSync = require('child_process').execSync
 
 async function fetchJSON({ http = require('https'), url }) {
   return new Promise((resolve, reject) => {
-    http.get(url, res => {
-      let data = ''
-      res.on('data', chunk => (data += chunk))
-      res
-        .on('end', () => {
-          try {
-            resolve(JSON.parse(data))
-          } catch (e) {
-            reject(e)
-          }
-        })
-        .on('error', error => {
-          reject(error)
-        })
-    })
+    try {
+      const req = http.get(url, res => {
+        let data = ''
+        res.on('data', chunk => (data += chunk))
+        res
+          .on('end', () => {
+            try {
+              resolve(JSON.parse(data))
+            } catch (e) {
+              reject(e)
+            }
+          })
+          .on('error', error => {
+            reject(error)
+          })
+      })
+      req.on('error', error => {
+        reject(error)
+      })
+    } catch (error) {
+      reject(error)
+    }
   })
 }
 
@@ -32,7 +39,7 @@ async function postJSON({
       ['_data', 'routes/_content/refresh-content'],
     ])
     const options = {
-      hostname: 'remix-fly-region-test.fly.dev',
+      hostname: `${process.env.FLY_APP_NAME}.fly.dev`,
       port: 443,
       path: `/_content/refresh-content?${searchParams}`,
       method: 'POST',
